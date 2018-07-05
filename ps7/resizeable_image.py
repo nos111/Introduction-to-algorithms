6c04e755078840c7c4b71081b29999d897cc8601
Nour <nsaffour@gmail.com>
2018-07-05 14:12:42 +0200

    fixed core issues and passed test large

--- ps7/resizeable_image.py
+++ ps7/resizeable_image.py
@@ -1,77 +1,76 @@
 import imagematrix
 import sys
 energyDict = {}
+track = {}
+dp = {}
 class ResizeableImage(imagematrix.ImageMatrix):
     def best_seam(self):
         energyDict = {}
-        bestSeamList = []
-        neighborsList = []
-        temp = ()
-        dp = []
-        for j in range(0, self.width - 1):
-            bestSeamList = []
-            dp.append([])
-            dp[j].append(0)
-            bestSeamList.append((j,0))
-            bestSeamList.append((j,1))
-            for i in range(1, self.height - 1):
-                #print("I is ",i)
-                #print("currently looking at ", bestSeamList[i])
-                neighborsList = self.getNeighbors(bestSeamList[i][0], bestSeamList[i][1])
-                tempEnerg = self.getEnergy(bestSeamList[i][0], bestSeamList[i][1])
-                temp = self.findMinEnergyNeighbors(neighborsList, tempEnerg)
-                dp[j][0] = dp[j][0] + tempEnerg
-                print("the neighbors are ", temp)
-                bestSeamList.append(temp)
-
-            dp[j].append(bestSeamList)
-        lowestValue = sys.maxint
-        index = 0
-        for x in range(0, len(dp)):
-            temp = dp[x][0]
-            if lowestValue > temp:
-                lowestValue = temp
-                index = x
-
-        return dp[index][1]
+        track = {}
+        dp = {}
+        startCord = ()
+        bestSeam = []
+        self.fillFirstRow()
+        for j in range(1, self.height):
+            for i in range(0, self.width):
+                self.updateNeighborEnergy(i,j)
+        startCord = self.findLowestValue()
+        bestSeam = self.buildTrack(startCord)
+        return bestSeam
 
     def remove_best_seam(self):
         self.remove_seam(self.best_seam())
 
-    def findLowestEnergyTop(self):
-        lowestEnergy = self.energy(1,1)
-        for x in range(1, self.width - 1):
-            if(self.getEnergy(x,1) < lowestEnergy):
-                lowestEnergy = self.getEnergy(x,1)
-        return lowestEnergy
+    def fillFirstRow(self):
+        for x in range(0, self.width ):
+            dp[x,0] = self.getEnergy(x,0)
+        
     def getEnergy(self, x, y):
         try:
             return energyDict[(x,y)]
         except KeyError:
             energyDict[(x,y)] = self.energy(x,y)
             return energyDict[(x,y)]
+
     def getNeighbors(self, x, y):
         if x == self.width - 1:
-            return [(x,y+1), (x-1, y+1)]
+            return [(x,y-1), (x-1, y-1)]
         elif x == 0:
-            return [(x, y+1), (x+1, y+1)]
-        #print("the start pixel coordinates is ", x, " ", y)
-        return [(x-1,y+1), (x, y+1), (x+1, y+1)]
+            return [(x, y-1), (x+1, y-1)]
+        return [(x-1,y-1), (x, y-1), (x+1, y-1)]
 
-    def findMinEnergyNeighbors(self, neighborList, currentPixEnergy):
-        print("the neighbors are  ", neighborList)
-        lowestEnergy = currentPixEnergy + self.getEnergy(neighborList[0][0], neighborList[0][1])
-        cordinate = (neighborList[0][0], neighborList[0][1])
-        for cor in neighborList:
-            
-            energy = self.getEnergy(cor[0], cor[1])
-            print("energy is ",energy + currentPixEnergy)
-            if(energy + currentPixEnergy > lowestEnergy):
-                lowestEnergy = energy + currentPixEnergy
-                cordinate = cor
-        print("\n")
-        return cordinate
+    def updateNeighborEnergy(self, x, y):
+        neighborsList = self.getNeighbors(x, y)
+        currentPixEnerg = self.getEnergy(x,y)
+        dp[x,y] = currentPixEnerg + dp[x,y - 1]
+        track[x,y] = 0
+        for cor in neighborsList:
+            energy = dp[cor[0], cor[1]]
+            if(energy + currentPixEnerg < dp[x,y]):
+                dp[x,y] = energy + currentPixEnerg
+                track[x,y] = x - cor[0]
+        #print "updated ", x , y, "with track ", track[x,y]
 
+    def findLowestValue(self):
+        lowest = dp[0, self.height - 1]
+        coordinates = (0, self.height - 1)
+        for x in range(0, self.width - 1):
+            if(dp[x,self.height - 1] < lowest):
+                lowest = dp[x,self.height - 1]
+                coordinates = (x,self.height - 1)
+        print("lowest value ", lowest)
+        return coordinates
+
+    def buildTrack(self, startCoord):
+        bestTrack = []
+        #print "start coordinates ", startCoord
+        bestTrack.append(startCoord)
+        for i in range(0, self.height - 1):
+            x = track[bestTrack[i][0], bestTrack[i][1]]
+            #print "adding ", (bestTrack[i][0] - x, bestTrack[i][1] - 1)
+            #print "x is ", x
+            bestTrack.append((bestTrack[i][0] - x, bestTrack[i][1] - 1))
+        return bestTrack
 
 
 
